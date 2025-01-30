ec0c6add5747634a0377d6897e00d90ae73c464e
Emile Schlick <emile.schlick@gmail.com>
2025-01-30 12:06:13 +0100

    Structuring up the project

--- src/main/java/org/example/pokemonservlet/database/DB.java
+++ src/main/java/org/example/pokemonservlet/database/DB.java
@@ -1,4 +1,24 @@
 package org.example.pokemonservlet.database;
 
+import org.example.pokemonservlet.models.Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DB {
+
+    public static List<Pokemon> pokemons = new ArrayList<>();
+
+    static{
+        pokemons.add(new Pokemon(1, "Pikachu","Elektric","Yellow"));
+        pokemons.add(new Pokemon(2, "Charmander", "Fire", "Orange"));
+        pokemons.add(new Pokemon(3, "Squirtle", "Water", "Blue"));
+        pokemons.add(new Pokemon(4, "Bulbasaur", "Grass", "Green"));
+        pokemons.add(new Pokemon(5, "Jigglypuff", "Normal", "Pink"));
+        pokemons.add(new Pokemon(6, "Gengar", "Ghost", "Purple"));
+        pokemons.add(new Pokemon(7, "Onix", "Rock", "Gray"));
+        pokemons.add(new Pokemon(8, "Alakazam", "Psychic", "Gold"));
+        pokemons.add(new Pokemon(9, "Machamp", "Fighting", "Blue-Gray"));
+        pokemons.add(new Pokemon(10, "Snorlax", "Normal", "Dark Blue"));
+    }
 }
--- src/main/java/org/example/pokemonservlet/models/Pokemon.java
+++ src/main/java/org/example/pokemonservlet/models/Pokemon.java
@@ -1,4 +1,48 @@
 package org.example.pokemonservlet.models;
 
 public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+    private String color;
+
+    public Pokemon(){}
+
+    public Pokemon(int id, String name, String type, String color){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
